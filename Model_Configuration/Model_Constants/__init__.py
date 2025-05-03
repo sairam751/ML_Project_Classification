97b35e96735714ee7500e7b40ced44254ede4956
sairam751 <sairam751348@gmail.com>
2025-05-03 19:01:01 +0530

    Renamed folders and config file

---
+++ Model_Configuration/Model_Constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH=Path("1.Model_Configuration/config.yaml")
+PARAMS_FILE_PATH = Path("1.Model_Configuration/params.yaml")
+SCHEMA_FILE_PATH = Path("1.Model_Configuration/schema.yaml")
---
+++ Model_Configuration/configuration.py
@@ -0,0 +1,2 @@
+from Model_Constants import *
+from Utils16.utils import read_yaml, create_directories, save_json, load_json
